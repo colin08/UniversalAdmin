139d94ed5aa176719b3aa67136ce098ead01c35f
litdev <litdev@outlook.com>
2016-08-16 18:09:54 +0800

    增加添加APP版本功能

--- Universal.DataCore/Entity/AppVersion.cs
+++ Universal.DataCore/Entity/AppVersion.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel;
 
 namespace Universal.DataCore.Entity
 {
@@ -8,14 +10,10 @@ namespace Universal.DataCore.Entity
     /// </summary>
     public enum APPVersionPlatforms
     {
-        /// <summary>
-        /// 安卓
-        /// </summary>
+        [Description("安卓")]
         Android = 1,
 
-        /// <summary>
-        /// 苹果
-        /// </summary>
+        [Description("苹果")]
         IOS = 2
     }
 
@@ -24,76 +22,91 @@ namespace Universal.DataCore.Entity
     /// </summary>
     public enum APPVersionType
     {
-        标准版 = 1,
-        企业版 = 2
+        [Description("标准版")]
+        Standard = 1,
+        [Description("企业版")]
+        Enterprise = 2
     }
 
     public class AppVersion
     {
         public int ID { get; set; }
-
-        /// <summary>
-        /// 所属平台
-        /// </summary>
+        
+        [Display(Name = "所属平台")]
         public APPVersionPlatforms Platforms { get; set; }
+
+        [NotMapped]
+        public string PlatformsStr
+        {
+            get
+            {
+                return Tools.EnumHelper.GetDescription<APPVersionPlatforms>(this.Platforms);
+            }
+        }
+
+        [NotMapped]
+        public string PlatformsLog
+        {
+            get
+            {
+                switch (Platforms)
+                {
+                    case APPVersionPlatforms.Android:
+                        return "fa fa-android";
+                    case APPVersionPlatforms.IOS:
+                        return "fa fa-apple";
+                    default:
+                        return "fa fa-question";
+                }
+            }
+        }
 
         /// <summary>
         /// 所属版本
         /// </summary>
+        [Display(Name = "版本类别")]
         public APPVersionType APPType { get; set; }
 
-        /// <summary>
-        /// 文件MD5
-        /// </summary>
-        [Required, MaxLength(100)]
+        [NotMapped]
+        public string APPTypeStr
+        {
+            get
+            {
+                return Tools.EnumHelper.GetDescription<APPVersionType>(this.APPType);
+            }
+        }
+        
+        [Required, MaxLength(100),Display(Name ="文件MD5值")]
         public string MD5 { get; set; }
-
-        /// <summary>
-        /// 大小
-        /// </summary>
-        [Required]
+        
+        [Required,Display(Name ="文件大小")]
         public long Size { get; set; }
 
         /// <summary>
         /// 版本
         /// </summary>
-        [Required, MaxLength(20)]
+        [Required, MaxLength(20),Display(Name ="版本号")]
         public string Version { get; set; }
-
-        /// <summary>
-        /// 升级号
-        /// </summary>
-        [Required]
+        
+        [Required,Display(Name ="升级号")]
         public int VersionCode { get; set; }
-
-        /// <summary>
-        /// logo地址
-        /// </summary>
-        [Required, MaxLength(255)]
+        
+        [Required, MaxLength(255),Display(Name ="Logo地址")]
         public string LogoImg { get; set; }
 
         /// <summary>
         /// 下载地址
         /// </summary>
-        [Required, MaxLength(255)]
+        [Required, MaxLength(255),Display(Name ="下载地址")]
         public string DownUrl { get; set; }
-
-        /// <summary>
-        /// 链接地址，IOS使用
-        /// </summary>
-        [MaxLength(500)]
+        
+        [MaxLength(500),Display(Name ="链接地址(IOS)")]
         public string LinkUrl { get; set; }
-
-        /// <summary>
-        /// 更新介绍
-        /// </summary>
-        [Required, MaxLength(500)]
+        
+        [Required, MaxLength(500),Display(Name ="更新介绍")]
         public string Content { get; set; }
-
-        /// <summary>
-        /// 发布时间
-        /// </summary>
-        [Required]
+        
+        [Required,Display(Name ="发布时间")]
         public DateTime AddTime { get; set; }
     }
 }
---
+++ Universal.Web/Areas/Admin/Controllers/AppVersionController.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+using Universal.Tools;
+using Universal.Web.Framework;
+
+namespace Universal.Web.Areas.Admin.Controllers
+{
+    public class AppVersionController : BaseAdminController
+    {
+        [AdminPermissionAttribute("APP版本", "APP版本首页")]
+        public ActionResult Index(int page = 1,int platform = 0)
+        {
+            LoadPlatform();
+
+            Models.ViewModelAppVersion response_model = new Models.ViewModelAppVersion();
+            response_model.page = page;
+            response_model.platform = platform;
+
+            //获取每页大小的Cookie
+            response_model.page_size = TypeHelper.ObjectToInt(WebHelper.GetCookie("appversionindex"), SiteKey.AdminDefaultPageSize);
+
+            var db = new DataCore.EFDBContext();
+            //查询分页
+            IQueryable<DataCore.Entity.AppVersion> query = db.AppVersions;
+            if (platform != 0)
+                query = query.Where(p => p.Platforms == (DataCore.Entity.APPVersionPlatforms)platform);
+
+            //总数
+            int total = query.Count();
+
+            query = query.OrderByDescending(p => p.AddTime);
+            query = query.Skip(response_model.page_size * (page - 1)).Take(response_model.page_size);
+
+            response_model.DataList = query.ToList();
+            response_model.total = total;
+            response_model.total_page = CalculatePage(total, response_model.page_size);
+            db.Dispose();
+            return View(response_model);
+        }
+
+        [HttpPost]
+        [AdminPermission("App版本", "删除版本")]
+        public JsonResult Del(string ids)
+        {
+            if (string.IsNullOrWhiteSpace(ids))
+            {
+                WorkContext.AjaxStringEntity.msgbox = "缺少参数";
+                return Json(WorkContext.AjaxStringEntity);
+            }
+
+            using (var db = new DataCore.EFDBContext())
+            {
+                db.Database.ExecuteSqlCommand("delete AppVersion where ID in('" + ids + "')");
+            }
+
+            WorkContext.AjaxStringEntity.msg = 1;
+            WorkContext.AjaxStringEntity.msgbox = "success";
+            return Json(WorkContext.AjaxStringEntity);
+        }
+
+        /// <summary>
+        /// 编辑安卓
+        /// </summary>
+        /// <returns></returns>
+        [AdminPermissionAttribute("App版本", "安卓版本编辑页面")]
+        public ActionResult EditAndroid(int? id)
+        {
+            using (var db = new DataCore.EFDBContext())
+            {
+                DataCore.Entity.AppVersion entity = new DataCore.Entity.AppVersion();
+                int num = TypeHelper.ObjectToInt(id, 0);
+                if (num != 0)
+                {
+                    entity = db.AppVersions.Find(num);
+                    if (entity == null)
+                        return PromptView("/admin/AppVersion", "404", "Not Found", "信息不存在或已被删除", 5);
+                    if(entity.Platforms != DataCore.Entity.APPVersionPlatforms.Android)
+                        return PromptView("/admin/AppVersion", "400", "数据非法", "此信息非安卓版本", 5);
+                }
+                
+                return View(entity);
+            }
+        }
+
+        [AdminPermission("App版本","保存安卓编辑信息")]
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public ActionResult EditAndroid(DataCore.Entity.AppVersion entity)
+        {
+            var isAdd = entity.ID == 0 ? true : false;
+
+            var db = new DataCore.EFDBContext();
+            LoadPlatform();
+            entity.APPType = DataCore.Entity.APPVersionType.Standard;
+            entity.Platforms = DataCore.Entity.APPVersionPlatforms.Android;
+
+            //数据验证
+            if (isAdd)
+            {
+                //判断版本是否存在
+                if (db.AppVersions.Count(p => p.APPType == DataCore.Entity.APPVersionType.Standard && p.Version == entity.Version) > 0)
+                {
+                    ModelState.AddModelError("Content", "该版本存在");
+                }
+
+            }
+            else
+            {
+                if (db.AppVersions.Count(p => p.ID == entity.ID) == 0)
+                {
+                    return PromptView("/admin/AppVersion", "404", "Not Found", "信息不存在或已被删除", 5);
+                }
+            }
+
+            if (ModelState.IsValid)
+            {
+                //添加
+                if (entity.ID == 0)
+                {
+                    entity.AddTime = DateTime.Now;
+                    db.AppVersions.Add(entity);
+
+                }
+                else //修改
+                {
+                    //var user = db.SysUsers.Find(entity.ID);
+                    //if (entity.Password != "litdev")
+                    //    user.Password = SecureHelper.MD5(entity.Password);
+                    //user.NickName = entity.NickName;
+                    //user.Gender = entity.Gender;
+                    //user.Status = entity.Status;
+                    //user.Avatar = entity.Avatar;
+                    //user.SysRoleID = entity.SysRoleID;
+                }
+
+                db.SaveChanges();
+                db.Dispose();
+                return PromptView("/admin/AppVersion", "OK", "Success", "操作成功", 5);
+            }
+            else
+            {
+                db.Dispose();
+                return View(entity);
+            }
+        }
+
+        /// <summary>
+        /// 编辑IOS
+        /// </summary>
+        /// <returns></returns>
+        [AdminPermissionAttribute("App版本", "IOS版本编辑页面")]
+        public ActionResult EditIOS(int? id)
+        {
+            using (var db = new DataCore.EFDBContext())
+            {
+                DataCore.Entity.AppVersion entity = new DataCore.Entity.AppVersion();
+                int num = TypeHelper.ObjectToInt(id, 0);
+                if (num != 0)
+                {
+                    entity = db.AppVersions.Find(num);
+                    if (entity == null)
+                        return PromptView("/admin/AppVersion", "404", "Not Found", "信息不存在或已被删除", 5);
+                    if (entity.Platforms != DataCore.Entity.APPVersionPlatforms.IOS)
+                        return PromptView("/admin/AppVersion", "400", "数据非法", "此信息非IOS版本", 5);
+                }
+
+                return View(entity);
+            }
+        }
+
+        /// <summary>
+        /// 加载平台列表
+        /// </summary>
+        private void LoadPlatform()
+        {
+            List<SelectListItem> platformList = new List<SelectListItem>();
+            platformList.Add(new SelectListItem() { Text = "所有平台", Value = "0" });
+            foreach (var item in EnumHelper.EnumToDictionary(typeof(DataCore.Entity.APPVersionPlatforms)))
+            {
+                string text = EnumHelper.GetDescription<DataCore.Entity.APPVersionPlatforms>((DataCore.Entity.APPVersionPlatforms)item.Key);
+                platformList.Add(new SelectListItem() { Text = text, Value = item.Key.ToString() });
+            }
+            ViewData["PlatformsList"] = platformList;
+
+            List<SelectListItem> typeList = new List<SelectListItem>();
+            typeList.Add(new SelectListItem() { Text = "所有类别", Value = "0" });
+            foreach (var item in EnumHelper.EnumToDictionary(typeof(DataCore.Entity.APPVersionType)))
+            {
+                string text = EnumHelper.GetDescription<DataCore.Entity.APPVersionType>((DataCore.Entity.APPVersionType)item.Key);
+                typeList.Add(new SelectListItem() { Text = text, Value = item.Key.ToString() });
+            }
+            ViewData["TypeList"] = typeList;
+        }
+    }
+}
