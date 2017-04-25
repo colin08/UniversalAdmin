942418fbba074d98151c2824c3a2740547c6ea9b
litdev <litdev@outlook.com>
2017-04-25 17:51:54 +0800

    增加推送测试方法

---
+++ Universal.Web/Controllers/TestController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Universal.Tools;
+using Universal.Web.Framework;
+
+namespace Universal.Web.Controllers
+{
+    /// <summary>
+    /// 测试
+    /// </summary>
+    public class TestController : BaseHBLController
+    {
+        // GET: Test
+        public ActionResult JPush(string alias)
+        {
+            Tools.JPush.PushALl(alias, "后台手动推送" + alias, 2, "4");
+
+            return Content("已推送");
+        }
+    }
+}
