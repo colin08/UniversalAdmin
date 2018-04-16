aea1c631031aecea807fb7d04ec183ef65f9255f
litdev <litdev@outlook.com>
2018-04-16 17:59:37 +0800

    添加定时计划项目

---
+++ TimerQuartzService/Job/StartJobHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimerQuartzService
+{
+    /// <summary>
+    /// 服务启动任务帮助类
+    /// </summary>
+    public class StartJobHelper
+    {
+        /// <summary>
+        /// 当服务启动时调用
+        /// </summary>
+        public static void Start()
+        {
+            //TODO 从数据库里查找任务，直接修改db数据
+            
+        }
+    }
+}
---
+++ TimerQuartzService/TimerService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.ServiceProcess;
+using Common.Logging;
+using Microsoft.Owin.Hosting;
+using System.Configuration;
+
+namespace TimerQuartzService
+{
+    public partial class TimerService : ServiceBase
+    {
+        static readonly ILog log = LogManager.GetLogger("JobLogAppender"); //日志信息
+
+        private IDisposable apiserver = null;
+
+        public TimerService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            string port = ConfigurationManager.AppSettings["WebAPIPort"].ToString();
+            //Services URI
+            string serveruri = "http://localhost:" + port + "/";
+            // Start OWIN host
+            apiserver = WebApp.Start<Startup>(url: serveruri);
+
+            log.Info("服务启动");
+
+            //启动所有的定时任务
+            QuartzManager<BaseJobObj>.startJobs();
+            //重新添加未完成的任务
+            StartJobHelper.Start();
+
+            log.Info("定时任务重新添加成功");
+        }
+
+        protected override void OnStop()
+        {
+            if (apiserver != null)
+                apiserver.Dispose();
+
+            log.Info("服务停止");
+            QuartzManager<BaseJobObj>.ShutdownJobs();
+            log.Info("定时任务已全部停止");
+        }
+    }
+}
