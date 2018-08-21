4544bc263783621c033f169921c51c7dc9ec7a8f
litdev <litdev@outlook.com>
2018-08-21 17:51:24 +0800

    后台编辑医生信息

--- Universal.Web/Areas/Admin/Controllers/MPDoctorController.cs
+++ Universal.Web/Areas/Admin/Controllers/MPDoctorController.cs
@@ -44,31 +44,87 @@ namespace Universal.Web.Areas.Admin.Controllers
         }
 
         /// <summary>
+        /// 选择诊所
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public ActionResult SelectClinic(int id)
+        {
+            ViewData["id"] = id;
+            BLL.BaseBLL<Entity.Clinic> bll = new BLL.BaseBLL<Entity.Clinic>();
+            var db_list = bll.GetListBy(0, p => p.Status, "Weight DESC");
+            List<SelectListItem> dataList = new List<SelectListItem>();
+            foreach (var item in db_list)
+            {
+                dataList.Add(new SelectListItem { Value = item.ID.ToString(), Text = item.Title });
+            }
+            ViewData["ClinicList"] = dataList;
+            return View();
+        }
+
+        /// <summary>
         /// 修改
         /// </summary>
         /// <param name="id">用户ID</param>
         /// <returns></returns>
-        [AdminPermissionAttribute("医生管理", "医生管理编辑页面")]
-        public ActionResult Edit(int? id)
+        [AdminPermissionAttribute("医生管理", "修改医生信息页面")]
+        public ActionResult Edit(int clinic_id,int id)
         {
-            if (id == null)
-            {
-                return PromptView("/admin/MPDoctor", "Error", "Error", "医生管理不支持后台添加", 5);
-            }
-
-            BLL.BaseBLL<Entity.MPUser> bll = new BLL.BaseBLL<Entity.MPUser>();
             Load();
-            Entity.MPUser entity = new Entity.MPUser();
-            int num = TypeHelper.ObjectToInt(id, 0);
-            if (num != 0)
+            var model = BLL.BLLMPDoctor.GetModel(id);
+            if(model == null) return PromptView("/admin/MPDoctor", "404", "Not Found", "信息不存在或已被删除", 5);
+            Models.ViewModelEditMPDoctor result = new Models.ViewModelEditMPDoctor();
+            result.id = id;
+            result.real_name = model.RealName;
+            result.Gender = model.Gender;
+            result.Status = model.Status;
+            result.telphone = model.Telphone;
+            result.Avatar = model.Avatar;
+            result.clinic_id = clinic_id;
+            string techang_ids = "";
+            string dep_ids = "";
+            if (model.DoctorsInfo == null)
             {
-                entity = bll.GetModel(p => p.ID == num, null);
-                if (entity == null)
+                result.can_adv = false;
+                result.adv_price = 99;
+                result.clinic_id = 1;
+                result.dep_ids = "";
+                result.touxian = "医师";
+                result.show_me = "";
+            }
+            else
+            {
+                result.can_adv = model.DoctorsInfo.CanAdvisory;
+                result.adv_price = model.DoctorsInfo.AdvisoryPrice;
+                if (model.DoctorsInfo.ClinicID == null) result.clinic_id = 1;
+                else result.clinic_id = TypeHelper.ObjectToInt(model.DoctorsInfo.ClinicID, 1);
+                result.dep_ids = "";
+                result.show_me = model.DoctorsInfo.ShowMe;
+                if(model.DoctorsInfo.DoctorsSpecialtyList!= null)
                 {
-                    return PromptView("/admin/MPDoctor", "404", "Not Found", "信息不存在或已被删除", 5);
+                    System.Text.StringBuilder str_techang = new System.Text.StringBuilder();
+                    foreach (var item in model.DoctorsInfo.DoctorsSpecialtyList.ToList())
+                    {
+                        str_techang.Append(item.ID.ToString() + ",");
+                    }
+                    techang_ids = str_techang.ToString();
+                }
+
+                if (model.DoctorsInfo.ClinicDepartmentList != null)
+                {
+                    System.Text.StringBuilder str_dep = new System.Text.StringBuilder();
+                    foreach (var item in model.DoctorsInfo.ClinicDepartmentList.ToList())
+                    {
+                        str_dep.Append(item.ID.ToString() + ",");
+                    }
+                    dep_ids = str_dep.ToString();
                 }
             }
-            return View(entity);
+            result.shanchang = techang_ids;
+            result.dep_ids = dep_ids;
+            LoadTeChang(techang_ids);
+            Loaddep(clinic_id, dep_ids);
+            return View(result);
         }
 
         /// <summary>
@@ -78,56 +134,36 @@ namespace Universal.Web.Areas.Admin.Controllers
         /// <returns></returns>
         [HttpPost]
         [ValidateAntiForgeryToken]
-        [AdminPermissionAttribute("医生管理", "保存医生管理编辑信息")]
-        public ActionResult Edit(Entity.MPUser entity)
+        [AdminPermissionAttribute("医生管理", "保存医生编辑信息")]
+        public ActionResult Edit(Models.ViewModelEditMPDoctor entity)
         {
-            var isAdd = entity.ID == 0 ? true : false;
-
-            BLL.BaseBLL<Entity.MPUser> bll = new BLL.BaseBLL<Entity.MPUser>();
             Load();
-
-            entity.Identity = Entity.MPUserIdentity.Doctors;
-
-            //数据验证
-            if (isAdd)
-            {
-
-            }
-            else
-            {
-                //如果要编辑的用户不存在
-                if (!bll.Exists(p => p.ID == entity.ID))
-                {
-                    return PromptView("/admin/MPDoctor", "404", "Not Found", "信息不存在或已被删除", 5);
-                }
-
-            }
-
+            LoadTeChang();
+            Loaddep(1);
+            
             if (ModelState.IsValid)
             {
-                //添加
-                if (entity.ID == 0)
-                {
+                var old_model = BLL.BLLMPDoctor.GetModel(entity.id);
+                old_model.RealName = entity.real_name;
+                old_model.Telphone = entity.telphone;
+                old_model.Gender = entity.Gender;
+                old_model.IsFullInfo = true;
+                if (old_model.DoctorsInfo == null) old_model.DoctorsInfo = new Entity.MPUserDoctors();
+                old_model.DoctorsInfo.ShowMe = entity.show_me;
+                old_model.DoctorsInfo.TouXian = entity.touxian;
+                old_model.DoctorsInfo.ClinicID =entity.clinic_id;
+                old_model.DoctorsInfo.CanAdvisory = entity.can_adv;
+                old_model.DoctorsInfo.AdvisoryPrice = entity.adv_price;
+                string msg = "";
 
 
-                }
-                else //修改
+                bool status = BLL.BLLMPDoctor.Modify(old_model, entity.shanchang, entity.dep_ids, out msg);
+                if (!status)
                 {
-                    var user = bll.GetModel(p => p.ID == entity.ID, null);
-                    user.Brithday = entity.Brithday;
-                    user.IDCardNumber = entity.IDCardNumber;
-                    user.IsFullInfo = true;
-                    user.RealName = entity.RealName;
-                    user.Telphone = entity.Telphone;
-                    user.Weight = entity.Weight;
-                    user.Gender = entity.Gender;
-                    user.Status = entity.Status;
-                    user.Avatar = entity.Avatar;
-                    user.Identity = entity.Identity;
-                    bll.Modify(user);
+                    return PromptView("/admin/MPDoctor/Edit?clinic_id=" + entity.clinic_id + "&id=" + entity.id, "404", "Not Found", "信息不存在或已被删除", 5);
                 }
 
-                return PromptView("/admin/MPDoctor", "OK", "Success", "操作成功", 5);
+                return PromptView("/admin/MPDoctor", "OK", "Success", "修改成功", 5);
             }
             else
                 return View(entity);
@@ -170,6 +206,50 @@ namespace Universal.Web.Areas.Admin.Controllers
                 dataList.Add(new SelectListItem() { Text = item.Title, Value = item.ID.ToString() });
             }
             ViewData["ClinicList"] = dataList;
+
+            
+        }
+
+        /// <summary>
+        /// 特长
+        /// </summary>
+        private void LoadTeChang(string default_val="")
+        {
+            //特长
+            string temp_def_val = "," + default_val.Replace(" ", "") + ",";
+            List<SelectListItem> dataList = new List<SelectListItem>();
+            BLL.BaseBLL<Entity.DoctorsSpecialty> bll = new BLL.BaseBLL<Entity.DoctorsSpecialty>();
+            var db_list = bll.GetListBy(0, p => p.ID > 0, "AddTime DESC");
+            foreach (var item in db_list)
+            {
+                string temp_val = "," + item.ID.ToString() + ",";
+                if (temp_def_val.IndexOf(temp_val) != -1)
+                    dataList.Add(new SelectListItem() { Text = item.Title, Value = item.ID.ToString(), Selected = true });
+                else
+                    dataList.Add(new SelectListItem() { Text = item.Title, Value = item.ID.ToString() });
+            }
+            ViewData["shanchangList"] = dataList;
+        }
+
+        /// <summary>
+        /// 科室
+        /// </summary>
+        private void Loaddep(int clinic_id,string default_val = "")
+        {
+            //特长
+            string temp_def_val = "," + default_val.Replace(" ", "") + ",";
+            List<SelectListItem> dataList = new List<SelectListItem>();
+            BLL.BaseBLL<Entity.ClinicDepartment> bll = new BLL.BaseBLL<Entity.ClinicDepartment>();
+            var db_list = bll.GetListBy(0, p => p.ClinicID == clinic_id && p.Status, "Weight DESC");
+            foreach (var item in db_list)
+            {
+                string temp_val = "," + item.ID.ToString() + ",";
+                if (temp_def_val.IndexOf(temp_val) != -1)
+                    dataList.Add(new SelectListItem() { Text = item.Title, Value = item.ID.ToString(), Selected = true });
+                else
+                    dataList.Add(new SelectListItem() { Text = item.Title, Value = item.ID.ToString() });
+            }
+            ViewData["depList"] = dataList;
         }
 
     }
---
+++ Universal.Web/Areas/Admin/Models/ViewModelEditMPDoctor.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Universal.Web.Areas.Admin.Models
+{
+    /// <summary>
+    /// 编辑医生信息
+    /// </summary>
+    public class ViewModelEditMPDoctor
+    {
+        public ViewModelEditMPDoctor()
+        {
+            this.touxian = "医师";
+        }
+
+        public int id { get; set; }
+
+        /// <summary>
+        /// 真实姓名
+        /// </summary>
+        [MaxLength(100),Required(ErrorMessage ="真实姓名必填")]
+        public string real_name { get; set; }
+
+        /// <summary>
+        /// 手机号
+        /// </summary>
+        [MaxLength(13),Required(ErrorMessage ="手机号必填")]
+        public string telphone { get; set; }
+
+        /// <summary>
+        /// 头像
+        /// </summary>
+        public string Avatar { get; set; }
+
+        /// <summary>
+        /// 性别
+        /// </summary>
+        public Entity.MPUserGenderType Gender { get; set; }
+        
+        /// <summary>
+        /// 状态
+        /// </summary>
+        public bool Status { get; set; }
+
+        /// <summary>
+        /// 所属诊所
+        /// </summary>
+        [Required(ErrorMessage ="请选择所属诊所")]
+        public int clinic_id { get; set; }
+
+        /// <summary>
+        /// 所属科室
+        /// </summary>
+        [Required(ErrorMessage ="请选择所属科室")]
+        public string dep_ids { get; set; }
+
+        /// <summary>
+        /// 头衔
+        /// </summary>
+        public string touxian { get; set; }
+
+        /// <summary>
+        /// 特长标签
+        /// </summary>
+        [Required(ErrorMessage ="请选择特长标签")]
+        public string shanchang { get; set; }
+
+        /// <summary>
+        /// 医师简介
+        /// </summary>
+        [Required(ErrorMessage ="请填写医师简介")]
+        public string show_me { get; set; }
+
+        /// <summary>
+        /// 是否可以资讯
+        /// </summary>
+        public bool can_adv { get; set; }
+
+        /// <summary>
+        /// 资讯价格
+        /// </summary>
+        public decimal adv_price { get; set; }
+
+    }
+}
